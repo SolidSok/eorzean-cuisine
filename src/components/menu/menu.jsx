26358da6268c03f4921b1523fa1609a9b133e67d
SolidSok <solidsok@gmail.com>
2023-01-02 23:53:23 -0800

    implemented menu style revisions

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './menu.css';
 import MenuCard from './menuCard/menuCard';
-import mockData from '../../mockData';
 import { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 
 export default function Menu() {
   const [food, setFood] = useState([]);
+  const breakfastImage = (
+    <img href="https://images.squarespace-cdn.com/content/v1/5bf4a3a07e3c3abf46ab8bdd/1570568873820-W2CT27ALLVTIF0JMREU0/00100lPORTRAIT_00100_BURST20191007182121684_COVER.jpg?format=1000w" />
+  );
 
   const getFood = () => {
     axios
@@ -40,6 +42,7 @@ export default function Menu() {
       <MenuCard
         food={food.filter(food => food.type === 'breakfast')}
         name="Breakfast"
+        image={breakfastImage}
       />
       <MenuCard
         food={food.filter(food => food.type === 'soups')}
--- src/components/menu/menuCard/menuCard.jsx
+++ src/components/menu/menuCard/menuCard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SampleImage from '../sampleImage';
 import './menuCard.css';
 
-export default function MenuCard({ food, name }) {
+export default function MenuCard({ food, name, image }) {
   return (
     <div className="menu">
-      <h1>
-        <span>{name}</span>
-      </h1>
+      <h2 className="category-header">
+        <span className="category">{name}</span>
+      </h2>
+      {image}
+      <p> </p>
       <SampleImage />
       <div className="menu-section">
         {' '}
@@ -18,20 +20,13 @@ export default function MenuCard({ food, name }) {
               className="menu-item"
               key={food._id}>
               <b>
-                <p>
+                <p className="food-name">
                   <span>{food.name}</span>{' '}
                 </p>
-              </b>
-              <ul>
-                <li>
-                  {' '}
-                  <p> {food.description}</p>
-                </li>
-                <li>
-                  {' '}
-                  <p>Popular in: {food.region}</p>
-                </li>
-              </ul>
+              </b>{' '}
+              <p className="food-description"> {food.description}</p>{' '}
+              <p className="food-description">Popular in: {food.region}</p>
+              <p className="price">Price: {food.price}</p>
             </div>
           );
         })}
